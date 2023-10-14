040409d564fc4b17e2fcccf684483f80d16b81e7
Chameleon Cloud User <cc@tp-head.novalocal>
2023-10-14 16:15:19 +0000

    lmao

---
+++ startup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo chmod +rws /home/cc
+
+sudo getent group munge || sudo groupadd -g 1011 munge
+sudo id -u munge || sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u 1001 -g munge  -s /sbin/nologin munge
+sudo getent group slurm || sudo groupadd -g 1012 slurm
+sudo id -u slurm || sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u 1002 -g slurm  -s /bin/bash slurm
+
+sudo umount -a
+
+mkdir -p /home/cc/container
+cc-cloudfuse mount /home/cc/container
+cp /home/cc/container/TeamPhoenix/hostfile ~/hostfile
+
+if [[ $1 = '--client' ]]; then
+	sudo yum install -y nfs-utils nfs-utils-lib
+	
+	mkdir -p /home/cc/intel
+	sudo mount -t nfs 10.140.81.187:/home/cc/intel /home/cc/intel
+
+	mkdir -p /home/cc/apps
+	sudo mount -t nfs 10.140.81.187:/home/cc/apps /home/cc/apps
+
+	setupdir=/home/cc/apps/setup
+
+	mkdir -p /home/cc/hpl
+	sudo mount -t nfs 10.140.81.187:/home/cc/hpl home/cc/hpl
+
+	sudo $setupdir/setup-compute-node
+else
+	cat hosts | xargs -i{} bash -c "ssh-keyscan -H {} >> ~/.ssh/known_hosts"
+fi
+
