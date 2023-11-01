f636a050da825bcae928dbb89339cf58525d63f0
jiyav <93935310+jiyav@users.noreply.github.com>
2023-11-01 10:47:49 -0400

    comments to gromacs_build.sh

--- gromacs_build.sh
+++ gromacs_build.sh
@@ -10,6 +10,7 @@ ml openmpi-gcc
 
 wget https://ftp.gromacs.org/gromacs/gromacs-2023.1.tar.gz
 
+#cmake args for mpi support, c/c++ compilers, haswell simd support, installation dir 
 cmake .. -DGMX_MPI=ON -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DMPI_C_COMPILER=mpicc -DMPI_CXX_COMPILER=mpicxx -DGMX_BUILD_OWN_FFTW=ON  -DGMX_GPU=OFF -DGMX_SIMD=AVX2_256 -DREGRESSIONTEST_DOWNLOAD=ON -DCMAKE_INSTALL_PREFIX=/home/cc/apps/gromacs
 
 
@@ -17,4 +18,5 @@ make -j24
 make -j24 check
 make install
 
+#source gmrc script to get access in current shell/onwards
 source /home/cc/apps/gromacs/bin/GMXRC
